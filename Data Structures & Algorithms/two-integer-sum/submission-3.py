3a28d971bc82a679be417d0d7b97e542f513138a
Samuel Wu <samuelwu318@gmail.com>
2026-07-14 01:55:33 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        if len(nums) == 2: return [0, 1]
+
+        for i in range(len(nums)):
+            req = target - nums[i]
+
+            if hash_table.get(req) is not None:
+                return [hash_table[req][0], i]
+
+            if hash_table.get(nums[i]) is None:
+                hash_table[nums[i]] = [i]
+            else:
+                hash_table[nums[i]].append(i)
+        
